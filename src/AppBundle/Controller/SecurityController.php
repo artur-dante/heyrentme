b554033a3178e5f78f3d205f2697218038ce83eb
Sebastian Bera <sebastian.bera@dantesoft.com>
2015-11-26 16:08:55 +0100

    users

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,70 @@
+<?php 
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Controller\SecurityController as BaseSecurityController;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
+class SecurityController extends BaseSecurityController
+{
+    public function loginAction(Request $request)
+    {
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
+
+        if (class_exists('\Symfony\Component\Security\Core\Security')) {
+            $authErrorKey = Security::AUTHENTICATION_ERROR;
+            $lastUsernameKey = Security::LAST_USERNAME;
+        } else {
+            // BC for SF < 2.6
+            $authErrorKey = SecurityContextInterface::AUTHENTICATION_ERROR;
+            $lastUsernameKey = SecurityContextInterface::LAST_USERNAME;
+        }
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has($authErrorKey)) {
+            $error = $request->attributes->get($authErrorKey);
+        } elseif (null !== $session && $session->has($authErrorKey)) {
+            $error = $session->get($authErrorKey);
+            $session->remove($authErrorKey);
+        } else {
+            $error = null;
+        }
+
+        if (!$error instanceof AuthenticationException) {
+            $error = null; // The value does not come from the security component.
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
+
+        if ($this->has('security.csrf.token_manager')) {
+            $csrfToken = $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue();
+        } else {
+            // BC for SF < 2.4
+            $csrfToken = $this->has('form.csrf_provider')
+                ? $this->get('form.csrf_provider')->generateCsrfToken('authenticate')
+                : null;
+        }
+                
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        if ($user != "anon." ){           
+            return $this->userIsLoggedAction();
+        }
+
+        return $this->renderLogin(array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'csrf_token' => $csrfToken,
+        ));
+    }
+    
+    public function userIsLoggedAction(){
+        $response = new Response("User_Is_Logged");
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+}
