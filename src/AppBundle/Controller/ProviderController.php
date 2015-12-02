28ebb97bdfe0dc24c72935f3ec7901773969c7c1
artur <artur@dantesoft.com>
2015-12-02 14:23:38 +0100

    equipment add 1: form validation

--- src/AppBundle/Controller/ProviderController.php
+++ src/AppBundle/Controller/ProviderController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Range;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
@@ -58,15 +59,44 @@ class ProviderController extends BaseController {
      * @Route("/provider/equipment-add-1/{subcategoryId}", name="equipment-add-1")
      */
     public function equipmentAdd1Action(Request $request, $subcategoryId) {
+        
+        // build form
+        //<editor-fold>
         $form = $this->createFormBuilder()
-                ->add('name', 'text')
-                ->add('price', 'money')
-                ->add('deposit', 'money')
-                ->add('value', 'money')
-                ->add('priceBuy', 'money', array('required' => false))
+                ->add('name', 'text', array(
+                    'constraints' => array(
+                        new NotBlank(),
+                        new Length(array('max' => 256))
+                    )
+                ))
+                ->add('price', 'money', array(
+                    'constraints' => array(
+                        new NotBlank(),
+                        new Range(array('min' => 0))
+                    )
+                ))
+                ->add('deposit', 'money', array(
+                    'constraints' => array(
+                        new NotBlank(),
+                        new Range(array('min' => 0))
+                    )
+                ))
+                ->add('value', 'money', array(
+                    'constraints' => array(
+                        new NotBlank(),
+                        new Range(array('min' => 0))
+                    )
+                ))
+                ->add('priceBuy', 'money', array(
+                    'required' => false,
+                    'constraints' => array(
+                        new Range(array('min' => 0))
+                    )
+                ))
                 ->add('invoice', 'checkbox', array('required' => false))
                 ->add('industrial', 'checkbox', array('required' => false))
                 ->getForm();
+        //</editor-fold>
         
         $form->handleRequest($request);
         
@@ -228,7 +258,7 @@ class ProviderController extends BaseController {
                 $file[2]);
             rename($file[3], $origFullPath);
             
-            // TODO: check image size
+            // check image size
             $imgInfo = getimagesize($origFullPath);
             $ow = $imgInfo[0]; // original width
             $oh = $imgInfo[1]; // original height
@@ -254,6 +284,7 @@ class ProviderController extends BaseController {
                 }
             }
             
+            // scale the image
             if ($scale) {
                 if ($file[2] == 'png') {
                     $img = imagecreatefrompng($origFullPath);
@@ -273,7 +304,7 @@ class ProviderController extends BaseController {
                 copy($origFullPath, $imgFullPath);
             }        
 
-            // create object
+            // store entry in database
             $img = new Image();
             $img->setUuid($file[0]);
             $img->setName($file[1]);
